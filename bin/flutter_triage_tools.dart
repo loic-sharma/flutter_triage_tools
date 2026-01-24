52f8d5d6340c0907da72d06aeda9c69ed64f32ab
Loïc Sharma <sharma.loic@gmail.com>
2026-01-24 14:31:43 -0800

    Start

---
+++ bin/flutter_triage_tools.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_triage_tools/flutter_triage_tools.dart' as flutter_triage_tools;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_triage_tools.calculate()}!');
+}
---
+++ test/flutter_triage_tools_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_triage_tools/flutter_triage_tools.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
